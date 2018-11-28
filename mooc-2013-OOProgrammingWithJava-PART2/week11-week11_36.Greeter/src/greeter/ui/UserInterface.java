d7134f07e174c6fdd5ce34b95762aa7603de3972
Jonas Eukan <jonas.eukan@gmail.com>
2018-11-28 23:15:33 +0800

    Finished submission

--- mooc-2013-OOProgrammingWithJava-PART2/week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
@@ -12,9 +12,18 @@ public class UserInterface implements Runnable {
 
     @Override
     public void run() {
+        frame = new JFrame();
+        frame.setTitle("Swing on");
+        frame.setPreferredSize(new Dimension(400, 200));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        createComponents(frame.getContentPane());
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private void createComponents(Container container) {
+        JLabel text = new JLabel("Hi!");
+        container.add(text);
     }
 
     public JFrame getFrame() {
--- mooc-2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
@@ -11,10 +11,38 @@ public class UserInterface implements Runnable {
     @Override
     public void run() {
         // Create your app here
+        frame = new JFrame();
+        frame.setPreferredSize(new Dimension(200, 300));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("Survey");
+        createComponents(frame.getContentPane());
+        frame.pack();
+        frame.setVisible(true);
     }
 
 
     public JFrame getFrame() {
         return frame;
+    }
+
+    private void createComponents(Container container) {
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        container.setLayout(layout);
+        
+        container.add(new JLabel("Are you?"));
+        container.add(new JCheckBox("Yes!"));
+        container.add(new JCheckBox("No!"));
+        container.add(new JLabel("Why?"));
+        
+        JRadioButton button1 = new JRadioButton("No reason.");
+        JRadioButton button2 = new JRadioButton("Because it is fun!");
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(button1);
+        group.add(button2);
+        
+        container.add(button1);
+        container.add(button2);
+        container.add(new JButton("Done!"));
     }
 }
