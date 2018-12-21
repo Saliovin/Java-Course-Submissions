d152dad047900657317998da844e9a9dfcbbed23
Jonas Eukan <jonas.eukan@gmail.com>
2018-12-21 17:54:38 +0800

    Finished submission

--- mooc-2013-OOProgrammingWithJava-PART2/week12-week12_48.StringBuilder/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week12-week12_48.StringBuilder/src/Main.java
@@ -1,12 +1,37 @@
 
+import java.util.Arrays;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] t = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] t = {1, 2, 3, 4};
         System.out.println(build(t));
     }
 
     public static String build(int[] t) {
-        return "{\n}\n";
+        StringBuilder string = new StringBuilder();
+        string.append("{\n ");
+        
+        for(int i = 0; i < t.length; i++) {
+            string.append(t[i] + ", ");
+            
+            if((i + 1) % 4 == 0) {
+                string.append("\n ");
+            }
+        }
+        
+        for(int i = string.length() - 1; i >= 0; i--) {
+            if(string.charAt(i) == ',') {
+                string.deleteCharAt(i);
+                break;
+            }
+            
+            string.deleteCharAt(i);
+        }
+        
+        string.append("\n}");
+        
+        return string.toString();
     }
 }
