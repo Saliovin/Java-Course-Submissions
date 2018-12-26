e15d00d71bd8546c8973199272cc464f3e904714
Jonas Eukan <jonas.eukan@gmail.com>
2018-12-26 19:22:14 +0800

    Added message for the creators

--- mooc-2013-OOProgrammingWithJava-PART2/week12-week12_50.CourseFeedback/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week12-week12_50.CourseFeedback/src/Main.java
@@ -6,5 +6,17 @@ public class Main {
                 "Course is over!\n" + 
                 "After you have submitted this exercise, remember to give us " + 
                 "some feedback about this course\n");
+        
+        /*
+        I would just like to say thank you for this wonderful course. I have 
+        learned so much from doing this course over the past months and it has
+        made me a better programmer.
+        As for feedback, the only thing I can say is that Part II of the course
+        needs to be rewritten as there were time when instructions were not
+        clear enough.
+        I do hope you continue in updating this amazing course as it will
+        undoubtedly help many more.
+        Thanks again for everything.
+        */
     }
 }
